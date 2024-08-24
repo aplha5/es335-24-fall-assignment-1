bc360bcf963f16b395ad7455e984d80ad7aec5e2
aplha5 <keerthiramineni@iitgn.ac.in>
2024-08-24 09:51:14 +0530

    update

--- classification-exp.py
+++ classification-exp.py
@@ -8,7 +8,7 @@ from sklearn.datasets import make_classification
 np.random.seed(42)
 
 # Code given in the question
-X, y = make_classification(
+X,y = make_classification(
     n_features=2, n_redundant=0, n_informative=2, random_state=1, n_clusters_per_class=2, class_sep=0.5)
 
 # For plotting
